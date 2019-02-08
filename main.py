eef47650910c938ac80ddf1c0d732b109652270f
Igor Tsallagov <igor.tsallagov@gmail.com>
2019-02-09 02:20:28 +0300

    Updated Mail methods

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import imaplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-header = None
-
 
 class Mail:
 
@@ -15,8 +13,7 @@ class Mail:
         self.imap = imap_address
         self.smtp = smtp_address
 
-    def create_letter(self, subject, message, *args):
-        recipients = args
+    def create_letter(self, subject, message, *recipients):
         letter = MIMEMultipart()
         letter['From'] = self.login
         letter['To'] = ', '.join(recipients)
@@ -24,41 +21,46 @@ class Mail:
         letter.attach(MIMEText(message))
         return letter
 
-    def send_mail(self, subject, message, *args):
-        letter = self.create_letter(subject, message, *args)
+    def send_mail(self, subject, message, *recipients):
+        letter = self.create_letter(subject, message, *recipients)
         mail_sender = smtplib.SMTP(self.smtp, 587)
 
-        # identify ourselves to SMTP client
+        # Identify ourselves to SMTP client
         mail_sender.ehlo()
 
-        # secure our email with TLS encryption
+        # Secure our email with TLS encryption
         mail_sender.starttls()
 
-        # re-identify ourselves as an encrypted connection
+        # Re-identify ourselves as an encrypted connection
         mail_sender.ehlo()
 
         mail_sender.login(self.login, self.password)
-        mail_sender.sendmail(self.login, args, letter.as_string())
+        mail_sender.sendmail(self.login, recipients, letter.as_bytes())
 
         mail_sender.quit()
 
-    def get_mail(self):
-        mail = imaplib.IMAP4_SSL(self.imap)
-        mail.login(self.login, self.password)
-        mail.list()
-        mail.select("inbox")
-        criterion = '(HEADER Subject "%s")' % header if header else 'ALL'
-        result, data = mail.uid('search', None, criterion)
+    def get_mail(self, header=None):
+        mail_getter = imaplib.IMAP4_SSL(self.imap)
+        mail_getter.login(self.login, self.password)
+        mail_getter.list()
+        mail_getter.select("INBOX")
+        if header:
+            criterion = f'(HEADER Subject "{header}")'
+        else:
+            criterion = 'ALL'
+        result, data = mail_getter.uid('search', None, criterion)
         assert data[0], 'There are no letters with current header'
         latest_email_uid = data[0].split()[-1]
-        result, data = mail.uid('fetch', latest_email_uid, '(RFC822)')
+        result, data = mail_getter.uid('fetch', latest_email_uid, '(RFC822)')
         raw_email = data[0][1]
-        email_message = email.message_from_string(raw_email)
-        mail.logout()
+        email_message = email.message_from_bytes(raw_email)
+        mail_getter.logout()
+        return email_message
 
 
-pi = Mail('pilyin1995@mail.ru', 'ela9Mequoo', 'imap.mail.ru', 'smtp.mail.ru')
+if __name__ == '__main__':
+    mailbox = Mail('python.d@rambler.ru', 'papfac-zivsUc-korcy7', 'imap.rambler.ru', 'smtp.rambler.ru')
+    mailbox.send_mail('Just Checking', 'Hello. I am just checking if this works.', mailbox.login)
 
-# pi.send_mail('Hello again', 'Updated code.', 'igor.tsallagov@gmail.com', 'ionathz@icloud.com')
-
-pi.get_mail()
+    email_to_get = mailbox.get_mail(header='Good Night')
+    print(email_to_get)
